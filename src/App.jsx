cc2c0014e732e2828c06028ccc1a9cb7381525e6
shawn <steadfastbae@gmail.com>
2023-04-09 17:08:55 -0500

    ~20:15

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,19 @@
+import { useState } from "react";
 import "./styles.css";
 export default function App() {
+  const [newItem, setNewItem] = useState("");
+
   return (
     <>
       <form className="new-item-form" action="">
         <div className="form-row">
           <label htmlFor="item">New Item</label>
-          <input type="text" id="item" />
+          <input
+            value={newItem}
+            onChange={(e) => setNewItem(e.target.value)}
+            type="text"
+            id="item"
+          />
         </div>
         <button className="btn">Add</button>
       </form>
